180e5b94697a5399b664f21df429691237801f6b
cjmcmurtrie <cj.mcmurtrie@gmail.com>
2018-08-23 20:11:33 +0100

    Euclidean vector distance loss

---
+++ utils/euclidean_loss.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+
+class EuclideanLoss(torch.nn.Module):
+    '''
+    Loss function treats pairwise distance between vectors as an error function.
+    Thus is is used to minimise Euclidean distance between inputs and targets.
+    If input is a batch of 2d tensors, tensors are flattened as vectors first.
+    '''
+
+    # todo: cuda support.
+    # todo: formalize normalization.
+
+    def __init__(self):
+        super(EuclideanLoss, self).__init__()
+        self.euclid = nn.PairwiseDistance()
+
+    def euclidean(self, inp, target):
+        if inp.dim() > 2:
+            inp = inp.contiguous().view(inp.size(0), inp.size(1) * inp.size(2))
+            target = target.contiguous().view(target.size(0), target.size(1) * target.size(2))
+        return self.euclid(inp, target)
+
+    def forward(self, inp, target):
+        dist = self.euclidean(inp, target)
+        loss = dist.sum() / dist.size(0)
+        return loss
+
+
